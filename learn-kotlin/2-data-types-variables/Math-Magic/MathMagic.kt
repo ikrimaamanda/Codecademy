b98ecc610ca137f1158e4b7824769ccf46cbe56f
ikrimaamanda <ikrima.amanda.07@gmail.com>
2020-09-15 12:59:09 +0700

    Create MathMagic.kt

---
+++ learn-kotlin/2-data-types-variables/Math-Magic/MathMagic.kt
@@ -0,0 +1,13 @@
+fun main() {
+  // this program converts the entered numbers into actual ages with magic tricks
+  
+  val userName = "Ikrima"
+  var magicNum : Int
+  magicNum = 2 * 5
+  magicNum += 3
+  magicNum *= 2
+  magicNum +=3
+  magicNum -= 6
+
+  println("$userName's age is $magicNum")
+}
